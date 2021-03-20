f0b499feac21db526d747f58f606cef73f3aa57f
Kirill Mecker <ghost3299@yandex.ru>
2021-03-20 20:07:03 +0400

    feat: style team

--- frontend/src/components/TeamCard/TeamCard.tsx
+++ frontend/src/components/TeamCard/TeamCard.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './TeamCard.scss'
 
 const info =
   'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
@@ -6,7 +7,11 @@ const info =
 const TeamCard: React.FC = () => {
   return (
     <article className="developer">
-      <img src="#" alt="#" />
+      <img
+        className="developer__photo"
+        src="https://dogcatdog.ru/wp-content/uploads/e/8/6/e866fd3696194ac57409ea72eb8a7b66.jpe"
+        alt="#"
+      />
       <div className="developer__info">
         <h3 className="developer__name">Name</h3>
         <p className="developer__profession">Web-developer</p>
