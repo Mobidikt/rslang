b0a3baeb8b04e6e67b4e587ce4408cbe3ae617ad
IKLOA <fandor1q@yandex.com>
2021-03-27 15:27:57 +0300

    fead: add axios interceptors response

--- frontend/src/axios/index.ts
+++ frontend/src/axios/index.ts
@@ -1,5 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import axios from 'axios'
+import { notification } from 'antd'
 import store from '../store'
+import actions from '../store/actions/auth'
 
 axios.interceptors.request.use((request) => {
   const state = store.getState()
@@ -12,3 +15,25 @@ axios.interceptors.request.use((request) => {
 
   return request
 })
+
+axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    const { status } = error.response
+
+    const state = store.getState()
+
+    if (status === 401 && state.authReducer.token) {
+      store.dispatch(actions.logout())
+      notification.warning({
+        message: 'Session Expired',
+        description:
+          'Logout from the Administration Panel was successful. To log in to Administration Panel, click on the "Sign in" button.',
+        duration: null,
+        key: 'session-expired',
+      })
+    }
+    return Promise.reject(error)
+  },
+)
