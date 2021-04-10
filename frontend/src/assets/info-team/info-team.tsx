ce74057411382bb201b4d5f223b1f0ca36e834a3
Kirill Mecker <ghost3299@yandex.ru>
2021-04-10 17:08:39 +0400

    feat: info our team

--- frontend/src/assets/info-team/info-team.tsx
+++ frontend/src/assets/info-team/info-team.tsx
@@ -14,7 +14,7 @@ const infoTeam: {
     photo: KirillPhoto,
     profession: 'Teamlead, web-developer',
     info:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      'В процессе работы над проектом отвечал за: построение архитектуры приложения; разработка титульной страницы приложения; видео презентация приложения; разработка общего компонентов для игр "настроки игры" и "статистика игры"; разработка игры "Аудиовызов"; улучшение качества кода, рефакторинг кода',
     git: 'https://github.com/Mobidikt',
   },
   {
@@ -22,7 +22,7 @@ const infoTeam: {
     photo: 'https://i.pinimg.com/originals/54/c6/68/54c6680d3a14edf2c20c654b4970fbec.jpg',
     profession: 'Web-developer',
     info:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      'В процессе работы над проектом отвечал за: авторизация, серверная валидация данных; хранение на сервере и вывод данных  о краткосрочной и долгосрочной статистики; построение графиков долгосрочной статистики, разработка игры "Саванна"; разработка Учебника и Словаря',
     git: 'https://github.com/IKLOA',
   },
   {
@@ -30,7 +30,7 @@ const infoTeam: {
     photo: AnzhelaPhoto,
     profession: 'Web-developer, Web-designer',
     info:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      'В процессе работы над проектом отвечала за: стилизацию приложения, разработка макетов в Figma; разработка игры "Наша игра"; разработка секции "О приложение"; доработка адаптива по всему приложению',
     git: 'https://github.com/anzhelaAbitova',
   },
   {
@@ -38,7 +38,7 @@ const infoTeam: {
     photo: ArtsemPhoto,
     profession: 'Web-developer',
     info:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      'В процессе работы над проектом отвечала за: страницу настроек; разработка общего компонента для игр "титульная страница игр"; разработка игры "Спринт"; разработка тестов; разработка секции "Видео".',
     git: 'https://github.com/ArtyomMakarov',
   },
 ]
