7d75b8dc4635639b990c288105ca581a43d58e0e
anzhelaAbitova <55100446+anzhelaAbitova@users.noreply.github.com>
2021-04-10 21:20:07 +0400

    fix: some adaptive layout fixes

--- frontend/src/components/Video/Video.tsx
+++ frontend/src/components/Video/Video.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import './Video.scss'
+import { useIntl } from 'react-intl'
 
 const Video: React.FC = () => {
   const videoSrc = 'https://www.youtube.com/embed/VSW7cSMdKiA'
+  const intl = useIntl()
 
   return (
     <section className="video" id="video">
       <div className="video__wrapper">
-        <h2 className="video__title">Video</h2>
+        <h2 className="video__title">{intl.formatMessage({ id: 'Video' })}</h2>
         <div className="video__iframe">
           <iframe
             title="video-country"
